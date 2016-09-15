d2ba7ee9b429873f5beaf097bfbcbc7c79da0119
Alden Peterson <alden.peterson@workiva.com>
2016-09-15 12:29:43 -0500

    python: make rpc calls inside try/except to handle errors in flush

--- test/expected/python.asyncio/f_Blah.py
+++ test/expected/python.asyncio/f_Blah.py
@@ -103,9 +103,9 @@ class Client(Iface):
         future = asyncio.Future()
         timed_future = asyncio.wait_for(future, timeout)
         await self._transport.register(ctx, self._recv_ping(ctx, future))
-        await self._send_ping(ctx)
 
         try:
+            await self._send_ping(ctx)
             result = await timed_future
         finally:
             await self._transport.unregister(ctx)
@@ -216,9 +216,9 @@ class Client(Iface):
         future = asyncio.Future()
         timed_future = asyncio.wait_for(future, timeout)
         await self._transport.register(ctx, self._recv_getThing(ctx, future))
-        await self._send_getThing(ctx)
 
         try:
+            await self._send_getThing(ctx)
             result = await timed_future
         finally:
             await self._transport.unregister(ctx)
@@ -268,9 +268,9 @@ class Client(Iface):
         future = asyncio.Future()
         timed_future = asyncio.wait_for(future, timeout)
         await self._transport.register(ctx, self._recv_getMyInt(ctx, future))
-        await self._send_getMyInt(ctx)
 
         try:
+            await self._send_getMyInt(ctx)
             result = await timed_future
         finally:
             await self._transport.unregister(ctx)
