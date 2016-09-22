8cdcdf235aeb345d2e9db98d0426d12cbab16b32
Alden Peterson <alden.peterson@workiva.com>
2016-09-22 17:25:47 -0500

    skynet: add validation for smithy artifacts as default skynet run

--- scripts/skynet/cross/setup_dart.sh
+++ scripts/skynet/cross/setup_dart.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 
 export FRUGAL_HOME=$GOPATH/src/github.com/Workiva/frugal
 
@@ -23,7 +23,16 @@ frugal_version=$(frugal --version | awk '{print $3}')
 # we need to manually install our package to match with the version of frugal
 # so delete existing package (if above pub get succeeded) and override with the
 # current version if not
+
 rm -rf  ~/.pub-cache/hosted/pub.workiva.org/frugal-${frugal_version}/
 mkdir -p ~/.pub-cache/hosted/pub.workiva.org/frugal-${frugal_version}/
-cp -r $FRUGAL_HOME/lib/dart/* ~/.pub-cache/hosted/pub.workiva.org/frugal-${frugal_version}/
+
+pub_extract_target=~/.pub-cache/hosted/pub.workiva.org/frugal-${frugal_version}/
+
+if [ -z "${IN_SKYNET_CLI+yes}" ]; then
+    tar -xzf ${SKYNET_APPLICATION_FRUGAL_PUB} -C $pub_extract_target
+else
+    cp -r $FRUGAL_HOME/lib/dart/* $pub_extract_target
+fi
+
 pub get --offline
