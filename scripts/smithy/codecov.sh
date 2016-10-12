e8e98a617e7761f021f7192280b65bca5fc3a1b7
Alden Peterson <alden.peterson@workiva.com>
2016-10-12 16:46:37 -0500

    WIP

--- scripts/smithy/codecov.sh
+++ scripts/smithy/codecov.sh
@@ -5,5 +5,12 @@ then
 	echo "GIT_BRANCH environment variable not set, skipping codecov push"
 else
 	TRACKING_REMOTE="$(git for-each-ref --format='%(upstream:short)' $(git symbolic-ref -q HEAD) | cut -d'/' -f1 | xargs git ls-remote --get-url | cut -d':' -f2 | sed 's/.git$//')"
+
+
+	# Dart
 	bash <(curl -s https://codecov.workiva.net/bash) -u https://codecov.workiva.net -t $CODECOV_TOKEN -r $TRACKING_REMOTE -f $FRUGAL_HOME/lib/dart/coverage/coverage.lcov -F dart_library
+
+	# Go library
+    bash <(curl -s https://codecov.workiva.net/bash) -u https://codecov.workiva.net -t $CODECOV_TOKEN -r $TRACKING_REMOTE -f $FRUGAL_HOME/gocoverage.txt -F go_library
+
 fi
