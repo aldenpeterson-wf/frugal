025bdf0c2f2e237c2149e67c781ee0390dc58106
Alden Peterson <alden.peterson@workiva.com>
2016-10-12 15:42:05 -0500

    WIP

--- scripts/smithy/codecov.sh
+++ scripts/smithy/codecov.sh
@@ -5,5 +5,5 @@ then
 	echo "GIT_BRANCH environment variable not set, skipping codecov push"
 else
 	TRACKING_REMOTE="$(git for-each-ref --format='%(upstream:short)' $(git symbolic-ref -q HEAD) | cut -d'/' -f1 | xargs git ls-remote --get-url | cut -d':' -f2 | sed 's/.git$//')"
-	bash <(curl -s https://codecov.workiva.net/bash) -u https://codecov.workiva.net -t $CODECOV_TOKEN -r $TRACKING_REMOTE -f ./coverage/coverage.lcov
+	bash <(curl -s https://codecov.workiva.net/bash) -u https://codecov.workiva.net -t $CODECOV_TOKEN -r $TRACKING_REMOTE -f ../../coverage/coverage.lcov
 fi
