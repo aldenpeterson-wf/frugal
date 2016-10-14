0c66619da67fbe8e210d9b125069609f36aa1f55
Alden Peterson <alden.peterson@workiva.com>
2016-10-14 11:30:58 -0500

    WIP

--- scripts/smithy/codecov.sh
+++ scripts/smithy/codecov.sh
@@ -13,7 +13,11 @@ else
 	# Go library
     bash <(curl -s https://codecov.workiva.net/bash) -u https://codecov.workiva.net -t $CODECOV_TOKEN -r $TRACKING_REMOTE -f $FRUGAL_HOME/gocoverage.txt -F go_library
 
+	# Java library
+    bash <(curl -s https://codecov.workiva.net/bash) -u https://codecov.workiva.net -t $CODECOV_TOKEN -r $TRACKING_REMOTE -f $FRUGAL_HOME/lib/java/target/site/jacoco/jacoco.xml -F java
+
+
     # Python2
-    bash <(curl -s https://codecov.workiva.net/bash) -u https://codecov.workiva.net -t $CODECOV_TOKEN -r $TRACKING_REMOTE -f $FRUGAL_HOME/lib/python/unit_tests_py2.xml -F python_two
+#    bash <(curl -s https://codecov.workiva.net/bash) -u https://codecov.workiva.net -t $CODECOV_TOKEN -r $TRACKING_REMOTE -f $FRUGAL_HOME/lib/python/unit_tests_py2.xml -F python_two
 
 fi
