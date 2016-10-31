019c997559680acf7ce9b7022b745442d2245198
Alden Peterson <alden.peterson@workiva.com>
2016-10-31 11:15:36 -0500

    WIP

--- scripts/smithy/codecov.sh
+++ scripts/smithy/codecov.sh
@@ -7,7 +7,6 @@ then
 else
 	TRACKING_REMOTE="$(git for-each-ref --format='%(upstream:short)' $(git symbolic-ref -q HEAD) | cut -d'/' -f1 | xargs git ls-remote --get-url | cut -d':' -f2 | sed 's/.git$//')"
 
-
 	# Dart
 	bash <(curl -s https://codecov.workiva.net/bash) -t $CODECOV_TOKEN -r $TRACKING_REMOTE -f $FRUGAL_HOME/lib/dart/coverage/coverage.lcov -F dartlibrarytest
 
@@ -16,9 +15,8 @@ else
 
 	# Java library
     bash <(curl -s https://codecov.workiva.net/bash) -t $CODECOV_TOKEN -r $TRACKING_REMOTE -f $FRUGAL_HOME/lib/java/target/site/jacoco/jacoco.xml -F java_library
-#
-#
-#    # Python2
-#    bash <(curl -s https://codecov.workiva.net/bash) -u https://codecov.workiva.net -t $CODECOV_TOKEN -r $TRACKING_REMOTE -f $FRUGAL_HOME/lib/python/unit_tests_py2.xml -F python_two
+
+    # Python2
+    bash <(curl -s https://codecov.workiva.net/bash) -u https://codecov.workiva.net -t $CODECOV_TOKEN -r $TRACKING_REMOTE -f $FRUGAL_HOME/lib/python/.coverage -F python_two
 
 fi
