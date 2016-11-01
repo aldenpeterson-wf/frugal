3ea627ad2fa108deff1ca84d41beaaa498925d00
Alden Peterson <alden.peterson@workiva.com>
2016-11-01 09:21:09 -0500

    WIP

--- scripts/smithy/codecov.sh
+++ scripts/smithy/codecov.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 echo "\nRunning codecov"
+
+if [[ -z $1 ]] || [[ -z $2 ]] ; then
+    echo "Codecov.sh requires both a filepath and a tag"
+    exit 1
+fi
+
 if [ -z "$GIT_BRANCH" ]
 then
 	echo "GIT_BRANCH environment variable not set, skipping codecov push"
