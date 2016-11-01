dbdda9576a9595be1f3f7fe4d7ae90cb0f9045d6
Alden Peterson <alden.peterson@workiva.com>
2016-11-01 10:32:14 -0500

    WIP

--- scripts/smithy/codecov.sh
+++ scripts/smithy/codecov.sh
@@ -13,6 +13,6 @@ then
 	echo "GIT_BRANCH environment variable not set, skipping codecov push"
 else
 	TRACKING_REMOTE="$(git for-each-ref --format='%(upstream:short)' $(git symbolic-ref -q HEAD) | cut -d'/' -f1 | xargs git ls-remote --get-url | cut -d':' -f2 | sed 's/.git$//')"
-    bash <(curl -s https://codecov.workiva.net/bash) -t $CODECOV_TOKEN -r $TRACKING_REMOTE -f $1 -F $2
+    bash <(curl -s https://codecov.workiva.net/bash) -t $CODECOV_TOKEN -r $TRACKING_REMOTE -f $1 -F $2 || echo "ERROR: Codecov failed to upload reports."
 
 fi
