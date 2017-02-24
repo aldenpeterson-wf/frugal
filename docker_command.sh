627816edda731bad8335962d2377652162fa476d
bender-wk <messaging@workiva.com>
2017-02-24 16:59:16 -0600

    Still failing

--- docker_command.sh
+++ docker_command.sh
@@ -2,4 +2,4 @@
 
 # docker run -it -v "/Users/aldenpeterson/.skynet-cli/.pub-cache":"/root/.pub-cache:rw"  -v "/Users/aldenpeterson/go/src/github.com/Workiva/frugal":"/frugal" drydock-prod.workiva.net/workiva/messaging-docker-images:119185 /bin/bash
 
-docker run -it -v "/Users/aldenpeterson/.skynet-cli/.cache/pip":"/root/.cache/pip:rw" -v "/Users/aldenpeterson/.skynet-cli/.pub-cache":"/root/.pub-cache:rw"  -v "/Users/aldenpeterson/go/src/github.com/Workiva/frugal":"/frugal" drydock-prod.workiva.net/workiva/messaging-docker-images:119185 /bin/bash
+docker run --name tester -it  -v "/Users/aldenpeterson/go/src/github.com/Workiva/frugal":"/frugal" drydock-prod.workiva.net/workiva/messaging-docker-images:119185 /bin/bash
