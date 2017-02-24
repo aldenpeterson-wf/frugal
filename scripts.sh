02f07ccd49607c0015a923a6eb9cfc40a925fe03
bender-wk <messaging@workiva.com>
2017-02-24 17:25:11 -0600

    Still failing

--- scripts.sh
+++ scripts.sh
@@ -1,17 +1,8 @@
 #!/usr/bin/env bash
 set -ex
 
-export FRUGAL_HOME=/frugal
-
-# Dart Dependencies
-#cd $FRUGAL_HOME/test/integration/dart/test_client
-#rm -rf .packages packages
-cd $FRUGAL_HOME
+cd /z
 pub upgrade
-#pub get --offline -v
 
-# Run cross tests - want to report any failures, so don't allow command to exit
-# without cleaning up
-cd ${FRUGAL_HOME}
 
 sleep 60
